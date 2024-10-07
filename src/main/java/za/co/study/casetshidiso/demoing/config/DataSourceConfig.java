d12bc2bd0d9a4a91d5e4e583770f785c075213f8
Tshidiso <tshidi,phaladi@gmail.com>
2024-10-07 23:53:09 +0200

    update datasourceconfig and persistence.xml

--- src/main/java/za/co/study/casetshidiso/demoing/config/DataSourceConfig.java
+++ src/main/java/za/co/study/casetshidiso/demoing/config/DataSourceConfig.java
@@ -1,25 +1,17 @@
 package za.co.study.casetshidiso.demoing.config;
 
 import jakarta.annotation.sql.DataSourceDefinition;
-import jakarta.annotation.sql.DataSourceDefinitions;
 import jakarta.ejb.Singleton;
 
+
+
 @Singleton
-@DataSourceDefinitions({
-        @DataSourceDefinition(
-                name = "java:app/datasource/userdatasource",
-                className = "com.mysql.cj.jdbc.MysqlDataSource",
-                url = "jdbc:mysql://mysqldb:3306/shop",
-                user = "shopuser",
-                password = "shopuserpass"
-        ),
-        @DataSourceDefinition(
-                name = "java:app/datasource/productdatasource",
-                className = "com.mysql.cj.jdbc.MysqlDataSource",
-                url = "jdbc:mysql://mysqldb:3306/shop",
-                user = "shopuser",
-                password = "shopuserpass"
-        )
-})
+@DataSourceDefinition(
+        name = "java:app/datasource/shopdatasource",
+        className = "com.mysql.cj.jdbc.MysqlDataSource",
+        url = "jdbc:mysql://mysqldb:3306/shop",
+        user = "shopuser",
+        password = "shopuserpass"
+)
 public class DataSourceConfig {
 }
